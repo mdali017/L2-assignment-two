bf817b7949d897712af53e0d28042f681674f617
mdali017 <mohabbatalit8@gmail.com>
2024-05-22 21:57:16 +0600

    Order related api created

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { productRouter } from './app/config/modules/products/product.route';
 import { OrderRoute } from './app/config/modules/orders/order.route';
+import { error } from 'console';
 const app: Application = express();
 
 // parser
@@ -11,6 +12,14 @@ app.use(cors());
 // application route
 app.use('/api/products', productRouter);
 app.use('/api/order', OrderRoute);
+
+// Not Found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(400).json({
+    success: false,
+    message: 'Route not found',
+  });
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Assignment Two.!');
